85de55fe895ba4d893df29d4febfa64b2886b2e4
parvez13913 <parvezz13913@gmail.com>
2023-08-04 12:06:13 +0600

    [DONE] book interface and model

---
+++ src/app/modules/book/book.model.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose';
+import { BookModel, IBook } from './book.interface';
+
+const bookSchema = new Schema<IBook, BookModel>({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  publicationDate: {
+    type: String,
+    required: true,
+  },
+  reviews: {
+    type: String,
+  },
+});
+
+export const Book = model<IBook, BookModel>('Book', bookSchema);
