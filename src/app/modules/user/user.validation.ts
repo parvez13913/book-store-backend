57fdddc8817ba0208331ba599a49b77823601087
parvez13913 <parvezz13913@gmail.com>
2023-08-06 18:41:24 +0600

    [DONE] user validation

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+const createUserZodSchema = z.object({
+  body: z.object({
+    name: z.object({
+      firstName: z.string({
+        required_error: 'First Name is Required',
+      }),
+      middleName: z.string().optional(),
+      lastName: z.string({
+        required_error: 'Last Name is Required',
+      }),
+    }),
+    email: z
+      .string({
+        required_error: 'Email is Required',
+      })
+      .email(),
+    password: z.string({
+      required_error: 'password is Required',
+    }),
+    phoneNumber: z.string().optional(),
+    address: z.string().optional(),
+  }),
+});
+
+export const UserValidation = {
+  createUserZodSchema,
+};
