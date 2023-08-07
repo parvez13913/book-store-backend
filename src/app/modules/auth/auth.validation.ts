7eaca9ec94d16063d87be5d4cecf03db6d7c46ff
parvez13913 <parvezz13913@gmail.com>
2023-08-07 13:12:10 +0600

    [DONE] auth validation

---
+++ src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+const loginZodSchema = z.object({
+  body: z.object({
+    email: z.string({
+      required_error: 'Email is Required',
+    }),
+    password: z.string({
+      required_error: 'Password is Required',
+    }),
+  }),
+});
+
+export const AuthValidation = {
+  loginZodSchema,
+};
